44f3cbb3410ce7488d1bbae41fd1699e82426878
Sadnuro <sadnr0213@gmail.com>
2021-08-27 00:59:30 -0500

    createGifos section updated with upload function
    se creó la capa hover de estado de subida y se aplicaron estilos

--- scripts/createGifo.js
+++ scripts/createGifo.js
@@ -1,32 +1,41 @@
-initInfo = `
+let initInfo = `
     <h3>Aquí podrás <br> crear tus propios <span class="text-resalt">GIFOS</span></h3>
     <p>¡Crea tu GIFO en solo 3 pasos! <br> (Solo necesitas una cámara para grabar un video)</p>
 `
 
-userMediaAccessInfo = `
+let userMediaAccessInfo = `
     <h3>¿Nos das acceso <br> a tu cámara?</h3>
     <p>El acceso a tu camara será válido sólo<br>por el tiempo en el que estés creando el GIFO.</p>
 `
-contentVideo = `
+let contentVideo = `
     <video src="" id="video"></video>
 `
 
-previewGif =`
+let previewGif =`
     <img id="gif-img" src="" alt="mygifo.gif">
+`
+let previewGifCapa = `
     <div id="createGifo-capa" >
-        <div id="status-img">Chulo</div>
+    <div class="statusinfo-container">
+        <div class="upload status-img" id="upload"></div>
         <p id="status-text">Estamos subiendo tu GIFO</p>
-
-        <div class="createGifo-containerBtns">
-            <div class="btn-download"></div>
-            <div class="btn-link"></div>
-        </div>
+    </div>
+    <div class="createGifo-containerBtns">
+    </div>
     </div>
 `
-timelapseTemplate = `
+let gifoSubidoTemplate = `
+    <div class="uploaded status-img" id="uploaded"></div>
+    <p id="status-text">GIFO subido con éxito</p>
+`
+let buttons_actions = `
+    <div class="btn-download"></div>
+    <div class="btn-link"></div>
+`
+let timelapseTemplate = `
     <p id="timelapse" class="timelapse"></p>
 `
-repeatRecordTemplate = `
+let repeatRecordTemplate = `
     <p id="repeatRecordBtn">REPETIR CAPTURA</p>
 `
 
@@ -36,24 +45,54 @@ const step3 = document.querySelector(".step3");
 
 const process_btn = document.querySelector("#process-btn");
 const infoDiv = document.querySelector(".info");
-let video;
-let img;
+let video = document.createElement('video');
+let img = document.createElement('img');
 const timelapse_container = document.querySelector(".timelapse-container");
-let timelapse;
+let timelapse = document.createElement('p');
 let repeatRecordBtn;
+let createGifoCapa;
 
-infoDiv.innerHTML = initInfo;
-process_btn.textContent = "CONTINUAR";
-
+let status = "NONE"
 let streamSignal;
 let recording;
 let gif;
+let gifId = "";
 
-secondsCounter = 0;
-minutesCounter = 0;
-seconds = '00';
-minutes = '00'
+ // Id asignado al gifo subido
+ secondsCounter = 0;
+ minutesCounter = 0;
+ seconds = '00';
+ minutes = '00'
 
+function initCreateGifosSection() {
+    console.log("Clear section")
+    infoDiv.innerHTML = initInfo;
+    process_btn.textContent = "CONTINUAR";
+
+    timelapse = document.createElement('p');
+    repeatRecordBtn = null;
+    createGifoCapa = null;
+
+    streamSignal = null;
+    recording = null;
+    gif = null;
+
+    video.remove();
+    img.remove();
+
+    step1.removeAttribute("data-active")
+    step2.removeAttribute("data-active")
+    step3.removeAttribute("data-active")
+
+    clearChronos();
+}
+
+// create_gifos_btn.onclick = initCreateGifosSection();
+create_gifos_btn.addEventListener("click", (event)=>{
+    if(status!="UPLOAD"){
+        initCreateGifosSection();
+    }
+})
 
 function clearChronos(){
     secondsCounter = 0;
@@ -62,6 +101,10 @@ function clearChronos(){
     minutes = '00';
     timelapse.textContent = minutes + ':' + seconds;
 }
+
+// clearChronos();
+// initCreateGifosSection();
+
 function chronos(){
     if (process_btn.textContent==="FINALIZAR") {
         
@@ -78,8 +121,10 @@ function chronos(){
         }
         secondsCounter<10? seconds = '0' + secondsCounter : seconds=secondsCounter;
 
+
         timelapse.textContent = `${minutes}:${seconds}`
 
+ 
         if (minutesCounter===1 && secondsCounter>=0 ){
             minutesCounter = 0;
             secondsCounter = 0;
@@ -94,6 +139,7 @@ const gifSetup = {type: 'gif', frameRate: 10, quality: 10, width: 360, height: 2
 process_btn.addEventListener("click", async (event)=>{
     switch (event.target.textContent){
         case "CONTINUAR":
+            status = "NONE"
             // change infoDiv | getUserMedia permisions
             infoDiv.innerHTML = userMediaAccessInfo;
             step1.setAttribute("data-active", true)
@@ -136,6 +182,9 @@ process_btn.addEventListener("click", async (event)=>{
             recording.stopRecording();
             clearInterval(chronos);
 
+            infoDiv.innerHTML = previewGif; 
+            img = document.querySelector("#gif-img"); // Show gif
+
             timelapse_container.innerHTML = repeatRecordTemplate;
             repeatRecordBtn = document.querySelector("#repeatRecordBtn");
             repeatRecordBtn.addEventListener("click", async (event)=>{
@@ -155,9 +204,6 @@ process_btn.addEventListener("click", async (event)=>{
 
             })
 
-            infoDiv.innerHTML = previewGif;
-            img = document.querySelector("#gif-img"); // Show gif
-
             gif = recording.getBlob();
 
             // Preview created gifo
@@ -168,33 +214,56 @@ process_btn.addEventListener("click", async (event)=>{
             process_btn.textContent = "SUBIR GIFO";
             break;
         case "SUBIR GIFO":
+            status = "UPLOAD"
+            process_btn.style = "display: none";
+
             // Create record.Blob() | fetch Giphy POST | 
             step2.removeAttribute("data-active");
             step3.setAttribute("data-active", true);
 
-            img.style = "display: none";
-            img.setAttribute("src", "");
+
+            timelapse_container.innerHTML = "";
+
+            // img.style = "display: none";
+            // img.setAttribute("src", "");
 
             // wait to resolve fetch upload          
             let formData = new FormData();
             formData.append('api_key', API_KEY);
             formData.append('file', gif, 'myGifo.gif');
 
+            infoDiv.innerHTML += previewGifCapa; 
+            createGifoCapa = document.querySelector(".statusinfo-container");
 
             let resultReq = undefined; 
-
             resultReq = await upload(gif);
-            console.log("resultReq.data.id:", resultReq.data.id)
+            const statusInfoContainer = document.querySelector(".statusinfo-container");
+            const createGifo_containerBtns = document.querySelector(".createGifo-containerBtns");
+            statusInfoContainer.innerHTML = gifoSubidoTemplate;   
+            createGifo_containerBtns.innerHTML = buttons_actions;
+
+            const GIFO_ID = resultReq.data.id;
+            console.log("gifoId:", GIFO_ID);
+
+            REQ_TYPE ="gifs";                // gifs | stickers
+            REQUEST = "trending";            // trending | search
+
+            URI = `${API_URL}/${REQ_TYPE}?api_key=${API_KEY}&ids=${GIFO_ID}`;
+            const myGifo = await search(URI);
+            myGifos.push(myGifo[0]);
+
+            console.log("myGifo:", myGifo);
+            console.log(myGifos)
+
 
             step3.removeAttribute("data-active")
-
-            infoDiv.innerHTML = initInfo;
 
             // invokeSaveAsDialog(gif);
             recording = null;
             gif = null;
             clearChronos();
 
+            status = "NONE"
             process_btn.textContent = "CONTINUAR"
             break;
     
@@ -205,6 +274,8 @@ process_btn.addEventListener("click", async (event)=>{
 })
 
 
+// const statusInfoContainer = document.querySelector(".statusinfo-container");
+// statusInfoContainer.innerHTML = gifoSubidoTemplate;
 
 
          
